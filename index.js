9e261b6b84f564eb2f3dd18fc33d7e4c61a1d713
Keith Getty <keithgetty@yahoo.com>
2017-09-10 19:26:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// write your code below!
+// write your code below
 function happyHolidays() {
   return "Happy holidays!"
-}
-
-function happyHolidaysTo(Neva) {
-  return `Happy holidays, ${Neva}!`
-}
-
-function happyHolidaysTo(holiday, Neva) {
-  return `Happy ${holiday}, ${Neva}!`
 }
